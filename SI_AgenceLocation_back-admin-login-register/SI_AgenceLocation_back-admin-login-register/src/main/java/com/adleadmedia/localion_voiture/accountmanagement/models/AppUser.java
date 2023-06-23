2f8dd9dd5127e3e426e48a7bdebf5b34b28dc364
Boufnichel314 <Boufnichel314@gmail.com>
2023-06-24 00:16:51 +0100

    appUser update to include the role too

--- SI_AgenceLocation_back-admin-login-register/SI_AgenceLocation_back-admin-login-register/src/main/java/com/adleadmedia/localion_voiture/accountmanagement/models/AppUser.java
+++ SI_AgenceLocation_back-admin-login-register/SI_AgenceLocation_back-admin-login-register/src/main/java/com/adleadmedia/localion_voiture/accountmanagement/models/AppUser.java
@@ -1,8 +1,10 @@
 package com.adleadmedia.localion_voiture.accountmanagement.models;
 
-
-import java.util.List;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,50 +12,47 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppUser {
+
 	@Id
 	@GeneratedValue
 	@Column(name = "user_id")
 	private Long userId;
+
 	@Column(nullable = false, length = 50)
 	private String email;
+
 	@Column(length = 30)
 	private String username;
+
 	@Column(nullable = false, length = 50)
 	private String firstName;
+
 	@Column(nullable = false, length = 50)
 	private String lastName;
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+
 	private String password;
+
 	@Column(nullable = false, length = 50)
 	private String phoneNumber;
+
 	@Column(length = 50)
 	private String picture;
-	
-	@Value("${statut:false}")
+
 	private boolean statut;
-	@JsonIgnore
-	@ManyToOne(fetch = FetchType.EAGER) @JsonIgnoreProperties("user")
-	@Cascade(CascadeType.ALL)
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JsonIgnoreProperties("users")
 	private AppRole roles;
 
 	public AppUser(String email, String username, String firstName, String lastName, String password, String phoneNumber, String picture) {
@@ -65,15 +64,15 @@ public class AppUser {
 		this.phoneNumber = phoneNumber;
 		this.picture = picture;
 	}
-	
-//	@OneToMany(mappedBy = "userApp") @JsonIgnoreProperties("userApp")
-//	@JsonIgnore
-//	private List<ActiviteLog> activites;
-	
-    @OneToOne(mappedBy = "user")
-    @Cascade(CascadeType.ALL)
-    @JsonIgnoreProperties
-	@JsonIgnore
-    private ActiviteLog activiteLog;
-}
 
+	@OneToOne(mappedBy = "user")
+	@JsonIgnoreProperties
+	@JsonIgnore
+	private ActiviteLog activiteLog;
+
+	public List<AppRole> getRoles() {
+		List<AppRole> roleList = new ArrayList<>();
+		roleList.add(roles);
+		return roleList;
+	}
+}
